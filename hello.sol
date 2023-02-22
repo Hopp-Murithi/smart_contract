3d1157aee302011468ba914fcc015503f52cbc65
Hopp Murithi <hoppmurithi@gmail.com>
2023-02-22 19:14:37 +0300

    feat: implement blockchain functions

---
+++ hello.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+contract Blockchain {
+    struct BlockStruck {
+        uint256 index;
+        uint256 timestamp;
+        uint256 amount;
+        address sender;
+        address recipient;
+    }
+
+    event BlockEvent(uint256 amount, address sender, address recipient);
+
+    BlockStruck[] chain;
+    uint256 chainCount;
+
+    constructor() {
+        chainCount = 0;
+    }
+
+    function addBlockToChain(uint256 amount, address payable recipient) public {
+        chainCount += 1;
+
+        chain.push(
+            BlockStruck(
+                chainCount,
+                block.timestamp,
+                amount,
+                msg.sender,
+                recipient
+            )
+        );
+
+        emit BlockEvent(amount, msg.sender, recipient);
+    }
+
+    function getChain() public view returns (BlockStruck[] memory) {
+        return chain;
+    }
+
+    function getChainCount() public view returns (uint256) {
+        return chainCount;
+    }
+}
+
+
